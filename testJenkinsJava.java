fc5838d8aeb90ee521de2ea4867bee4793a281fa
Damicode <damierraymond@yahoo.fr>
2020-05-30 11:04:08 -0400

    Yes
    Made it

--- testJenkinsJava.java
+++ testJenkinsJava.java
@@ -4,6 +4,6 @@ public class testJenkinsJava
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World With Jenkins "); 
+        System.out.println("Hello, World With Jenkins Today TESTING "); 
     } 
 } 
