9c0a9f46b8334742156f0898e6c6a6226e104b7c
Damicode <damierraymond@yahoo.fr>
2020-05-30 11:00:02 -0400

    Update testJenkinsJava.java

--- testJenkinsJava.java
+++ testJenkinsJava.java
@@ -4,6 +4,6 @@ public class testJenkinsJava
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World With Jenkins TODAY"); 
+        System.out.println("Hello, World With Jenkins "); 
     } 
 } 
